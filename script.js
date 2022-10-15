ae50395bcde7ee31d0eb0dd8e3c2e0605ece6bf7
RubsRafa <rubiarafaela2000@gmail.com>
2022-10-15 18:27:40 -0300

    versão quase lá

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
+let escolha1;
 function selecionarPrato (prato) {
     const selecaoPrato = document.querySelector (".pratos .borda");
     if ( selecaoPrato !== null) {
         selecaoPrato.classList.remove ("borda");
     }
     prato.classList.toggle ("borda");
+    escolha1 = prato.innerHTML;
+    finalizar (); 
 }
-
+let escolha2; 
 function selecionarBebida (bebida) {
     const selecaoBebida = document.querySelector (".bebidas .borda");
     if ( selecaoBebida !== null) {
         selecaoBebida.classList.remove ("borda");
     }
     bebida.classList.toggle ("borda");
+    escolha2 = bebida.innerHTML; 
+    finalizar (); 
 }
-
+let escolha3;
 function selecionarSobremesa (sobremesa) {
     const selecaoSobremesa = document.querySelector (".sobremesas .borda");
     if ( selecaoSobremesa !== null) {
         selecaoSobremesa.classList.remove ("borda");
     }
     sobremesa.classList.toggle ("borda");
+    escolha3 = sobremesa.innerHTML; 
+    finalizar (); 
 }
 
 
 function finalizar () {
-    const pedido = document.querySelector (".botao");
-    pedido.classList.add ("botaofinal");
-    pedido.classList.remove ("botao"); 
+    const fecharPedido = document.querySelector (".fechar")
+    const concluido = document.querySelector (".botao");
+    if (escolha1 != undefined) {
+        if (escolha2 != undefined) {
+            if (escolha3 != undefined) {
+                fecharPedido.classList.remove ("escondido");
+                concluido.classList.add ("escondido");
+            }
+        }
+    }
+}
+function pedir () {
+    const finalizarPedido = document.querySelector (".finalizar-pedido");
+    finalizarPedido.classList.remove ("escondido"); 
 
-
-    const text = document.querySelector (".texto");
-    text.classList.add ("texto2");
-    text.classList.remove ("texto")
-    text.innerHTML = "Fechar pedido";
+    escolha1.innerHTML = `${escolha1 (".nome")} + ${escolha1(".valor")}`
+    escolha2.innerHTML = "boa tade"; 
+    escolha3.innerHTML = "tudo bom"; 
 }
